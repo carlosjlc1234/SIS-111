f441d855f54dc57cac34dd4d11fe8fb8aa970e95
carlosjlc1234 <carlos.lopez.c1@ucb.edu.bo>
2025-04-10 09:58:43 -0400

    Add files via upload

---
+++ introducion programacion/operadores_aritmeticos.py
@@ -0,0 +1,59 @@
+
+#CREAR UNA VAR
+saludo = "Hola Mundo!!!"
+print(saludo)
+
+
+#suma de numeros
+num1 = 10
+num2 = 20
+suma = num1 + num2
+print (suma)
+
+#suma de palabras
+palabra1 = "hola"
+palabra2 = "mundo"
+sumapalabras = palabra1 + palabra2
+print (sumapalabras)
+
+
+#resta de numeros
+num1 = 5
+num2 = 25
+x = num1 - num2
+print (x)
+
+
+#resta de numeros
+num1 = 37
+num2 = 202386
+y = num1 - num2
+print (y)
+
+
+#multiplicacion de numeros
+num1 = 12
+num2 = 26
+e1 = num1 * num2
+print (e1)
+
+
+#multiplicacion de numeros
+num1 = 26
+num2 = 26
+e2 = num1 * num2
+print (e2)
+
+
+#division de numeros
+num1 = 27
+num2 = 43
+d1 = num1 / num2
+print (d1)
+
+
+#division de numeros
+num1 = 26
+num2 = 48
+d2 = num1 / num2
+print (d2)
